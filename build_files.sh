67a2fcd78e3394d4b0335c87652949afe6c73103
noyciy7037 <noyciy7037@gmail.com>
2026-03-01 00:43:19 +0900

    activate uv venv

--- build_files.sh
+++ build_files.sh
@@ -3,4 +3,5 @@ npm run build -w translate
 dnf install libpq-devel libxml2-devel libxslt-devel -y
 uv venv -p 3.11
 uv pip sync requirements.txt
+source .venv/bin/activate
 python manage.py collectstatic
